ffe1794152d00c0c46f1cb2f82cf22b83c72e96b
Silas Reinagel <silas.reinagel@carvana.com>
2020-06-17 16:46:32 -0700

    Improved Photo Swiper Responsiveness. Closes #6

--- src/Sections/PhotoSlider.svelte
+++ src/Sections/PhotoSlider.svelte
@@ -6,7 +6,7 @@
 <script>
   const initSlider = () => {
     var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 4,
+      slidesPerView: 'auto',
       spaceBetween: 0,
       pagination: {
         el: '.swiper-pagination',
@@ -18,29 +18,42 @@
 
 <div class="swiper-container">
   <div class="swiper-wrapper">
-    <div class="swiper-slide"><img src="/images/cafe-entrance-01.jpg" loading="lazy"/></div>
-    <div class="swiper-slide"><img src="/images/live-jazz-01.jpg" loading="lazy"/></div>
-    <div class="swiper-slide"><img src="/images/cafe-01.jpg" loading="lazy"/></div>
-    <div class="swiper-slide"><img src="/images/people-in-cafe-01.jpg" loading="lazy"/></div>
-    <div class="swiper-slide"><img src="/images/hours-sign.jpg" loading="lazy"/></div>
-    <div class="swiper-slide"><img src="/images/cafe-03.jpg" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/barista-02.jpg" alt="Cafe Pic" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/cafe-05.jpg" alt="Cafe Pic" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/live-jazz-04.jpg" alt="Cafe Pic" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/cafe-entrance-01.jpg" alt="Cafe Pic" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/cafe-06.jpg" alt="Cafe Pic"loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/live-jazz-03.jpg" alt="Cafe Pic" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/cafe-01.jpg"alt="Cafe Pic" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/cafe-03.jpg" alt="Cafe Pic" loading="lazy"/></div>
+    <div class="swiper-slide"><img src="/images/hours-sign.jpg" alt="Cafe Pic"loading="lazy"/></div>
   </div>
   <div class="swiper-pagination"></div>
 </div>
 
 <style>
   .swiper-container {
-    height: 330px;
+    height: calc(9 * max(320px, min(33vw, 640px)) / 16);
     background-color: var(--primary-background-color);
   }
 
   .swiper-wrapper div {
-    height: 330px;
-    width: 440px;
+    min-width: 320px;
+    width: 33vw;
+    max-width: 640px;
+    height: auto;
+    overflow: hidden;
+    text-align: center;
+    vertical-align: middle;
   }
 
-  img {
-    height: 100%;
-    width: auto;
+  @media screen and (max-width: 600px) {
+    .swiper-container {
+      height: calc(9 * 100vw / 16);
+    }
+
+    .swiper-wrapper div {
+      width: 100vw;
+    }
   }
 </style>
